cbd648bd6715d366777fa93c478c6f34c2e006be
Julian Montufar <julamontufar@misena.edu.co>
2024-05-14 22:31:33 -0500

    key value pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -35,4 +35,13 @@ export class UncommonPageComponent {
     this.clients.shift();
   }
 
+  //KeyValue Pipe
+
+  public person = {
+    name: 'Fernando',
+    age: 36,
+    address: 'Ottawa, Canada'
+
+  }
+
 }
